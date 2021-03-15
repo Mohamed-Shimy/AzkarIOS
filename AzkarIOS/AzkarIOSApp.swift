98ca240b88725073a9b7689014a89f3ddcc2e1e5
Mohamed Shemy <msh.work@outlook.sa>
2021-03-15 22:15:53 +0200

    Initial Commit

---
+++ AzkarIOS/AzkarIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzkarIOSApp.swift
+//  AzkarIOS
+//
+//  Created by Mohamed Shemy on Mon 15 Mar 2021.
+//
+
+import SwiftUI
+
+@main
+struct AzkarIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AzkarIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AzkarIOS
+//
+//  Created by Mohamed Shemy on Mon 15 Mar 2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
